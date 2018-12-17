20ca985f387a516ea37332d8d7c91643f1e8e5cc
harrystacey123 <harrystacey@live.co.uk>
2018-12-17 15:31:25 -0800

    started on little profile photo in nav bar

---
+++ learnify/static/js/main.js
@@ -0,0 +1,12 @@
+console.log('I am working');
+
+$('.forest').on('click', function() {
+    $('.forest').addClass('woods');
+})
+
+
+// $('#box2').on('click', function () {
+// 	$('h1').addClass('crazy');
+// 	$('.box').removeClass('crazy')
+// 	$('#box3').slideToggle();
+// });
