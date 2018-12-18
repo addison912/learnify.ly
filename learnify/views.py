33a3b6a78c94f9c03d45064430b56a2360bc5c19
harrystacey123 <harrystacey@live.co.uk>
2018-12-18 10:53:40 -0800

    adding register form to homepage

--- learnify/views.py
+++ learnify/views.py
@@ -8,7 +8,28 @@ from django.conf import settings
 
 
 def index(request):
-    return render(request, 'learnify/index.html')
+    registered = False
+    if request.method == 'POST':
+        user_form = UserForm(data=requrest.POST)
+        profile_form = UserProfileForm(data=request.POST)
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            profile.user = user
+            if 'profile_pic' in requrest.FILES:
+                profile.profile_pic = request.FILES['profile_pic']
+            profile.save()
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+    return render(
+        request, 'learnify/index.html', {
+            'user_form': user_form,
+            'profile_form': profile_form,
+            'registered': registered
+        })
 
 
 def courses(request):
