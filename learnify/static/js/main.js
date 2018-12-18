1a18d5fd7641bcab0fdbc3011adcb8e42329424c
harrystacey123 <harrystacey@live.co.uk>
2018-12-17 17:01:40 -0800

    continued dropdown menu

--- learnify/static/js/main.js
+++ learnify/static/js/main.js
@@ -1,8 +1,8 @@
 console.log('I am working');
 
-// $(document).ready(function() {
-//     $( '.bercow').hide();
-//     $('.forest').on('click', function() {
-//         $( '.bercow').slideToggle('slow');
-//     })
-// });
+$(document).ready(function() {
+    $( '.bercow').hide();
+    $('.forest').on('click', function() {
+        $( '.bercow').slideToggle('slow');
+    })
+});
