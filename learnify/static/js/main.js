cff25a2f32efba5d805eebaae5ac82d1e2b630f6
harrystacey123 <harrystacey@live.co.uk>
2018-12-20 09:37:24 -0800

    finishing hamburgerm menu

--- learnify/static/js/main.js
+++ learnify/static/js/main.js
@@ -1,8 +1,23 @@
-console.log('I am working');
-
 $(document).ready(function () {
     $('.nav-dropdown').hide();
     $('.nav-profile-pic').on('click', function () {
         $('.nav-dropdown').slideToggle('slow');
-    })
+    });
+
+    $('.cross').hide();
+    $('.hamburger-list').hide();
+    $('.hamburger').on('click', function() {
+        $('.hamburger-list').slideToggle('slow', function() {
+            $('.cross').show();
+            $('.hamburger').hide();
+        });
+    });
+
+    $('.cross').on('click', function() {
+        $('.hamburger-list').slideToggle('slow', function() {
+            $('.hamburger').show();
+            $('.cross').hide();
+        });
+    });
 });
+
